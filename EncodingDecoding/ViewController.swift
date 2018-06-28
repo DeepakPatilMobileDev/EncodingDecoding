5334fbe2df664b6913951d89f10e8b915476c100
Deepak P. Patil <dpatil@ios-dpatil.local>
2018-06-28 11:42:27 +0530

    Initial Commit

---
+++ EncodingDecoding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EncodingDecoding
+//
+//  Created by Deepak P. Patil on 28/06/18.
+//  Copyright © 2018 Deepak P. Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
